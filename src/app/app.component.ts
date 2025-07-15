99f4f2cb35426216164bc50f7ea25e0a6f22ecab
AnaBeatrizBentoSilva <anabento3807@gmail.com>
2025-07-15 14:06:45 -0300

    feat: design nav footer header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './components/template/nav/nav.component';
 import { FooterComponent } from "./components/template/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, FooterComponent],
+  imports: [NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
